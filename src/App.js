74d16949ae6c7da9ebba1d16a524fb30a0cfbbdb
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-11 16:10:57 +0300

    Main Navigation component added to App component

--- src/App.js
+++ src/App.js
@@ -6,18 +6,22 @@ import {
   Redirect,
 } from 'react-router-dom';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation/MainNavigation';
 // import NotFound from './shared/components/NotFound';
 import Users from './user/pages/Users';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path='/' component={Users} />
-        <Route exact path='/places/new' component={NewPlace} />
-        {/* <Route exact path='/404' component={NotFound} /> */}
-        <Redirect to='/' />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route exact path='/' component={Users} />
+          <Route exact path='/places/new' component={NewPlace} />
+          {/* <Route exact path='/404' component={NotFound} /> */}
+          <Redirect to='/' />
+        </Switch>
+      </main>
     </Router>
   );
 };
