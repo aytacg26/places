7c78f733d7cb5af1eeba376b5944ce30801e74b4
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-11 15:36:34 +0300

    UserItem Component Created

--- src/user/components/UserItem/UserItem.js
+++ src/user/components/UserItem/UserItem.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Avatar from '../../../shared/components/UIElements/Avatar/Avatar';
+import Card from '../../../shared/components/UIElements/Card/Card';
 import './UserItem.css';
 
-const UserItem = (props) => {
-  return <div>User Item which will be used in UsersList.</div>;
+const UserItem = ({ id, name, image, placeCount }) => {
+  return (
+    <li className='user-item'>
+      <Card className='user-item__content'>
+        <Link to={`/${id}/places`}>
+          <div className='user-item__image'>
+            <Avatar image={image} alt={name} title={name} />
+          </div>
+          <div className='user-item__info'>
+            <h2>{name}</h2>
+            {placeCount > 0 ? (
+              <h3>
+                {placeCount} {placeCount === 1 ? ' Place' : 'Places'}
+              </h3>
+            ) : (
+              <h4>No Places Shared</h4>
+            )}
+          </div>
+        </Link>
+      </Card>
+    </li>
+  );
 };
 
 export default UserItem;
