c69ff74092268a548f3bc9d2d4e29dd66f3dccdf
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-11 15:37:12 +0300

    Users Page Component Created and filled with dummy data

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import UsersList from '../components/UserList/UsersList';
 
 const User = () => {
-  return <div>User Page Example...</div>;
+  //This will be fetched from backend
+  const users = [
+    {
+      id: '1-anj-151-tk',
+      name: 'Aytac Guley',
+      places: 10,
+      image: 'https://i.ibb.co/sqdtZ8s/User-Profile-PNG.png',
+    },
+    {
+      id: '2-hjk-122-tr',
+      name: 'Aslı Guley',
+      places: 18,
+      image: 'https://i.ibb.co/sqdtZ8s/User-Profile-PNG.png',
+    },
+    {
+      id: '3-plu-132-kl',
+      name: 'Ziba Guley',
+      places: 25,
+      image: 'https://i.ibb.co/sqdtZ8s/User-Profile-PNG.png',
+    },
+    {
+      id: '4-ulk-120-jj',
+      name: 'Halil Guley',
+      places: 12,
+      image: 'https://i.ibb.co/sqdtZ8s/User-Profile-PNG.png',
+    },
+    {
+      id: '5-plo-198-tt',
+      name: 'Fatma Guley',
+      places: 0,
+      image: 'https://i.ibb.co/sqdtZ8s/User-Profile-PNG.png',
+    },
+  ];
+
+  return (
+    <div>
+      <UsersList users={users} />
+    </div>
+  );
 };
 
 export default User;
