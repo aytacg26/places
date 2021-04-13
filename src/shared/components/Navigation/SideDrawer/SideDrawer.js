9a9e108a85a107e54e33f91ef131b2ab9bebf697
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-13 22:05:31 +0300

    Side Drawer component created for small screens

---
+++ src/shared/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SideDrawer.css';
+
+const SideDrawer = (props) => {
+  return <aside className='side-drawer'>{props.children}</aside>;
+};
+
+export default SideDrawer;
