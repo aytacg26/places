904a5a665868a8a132d8d8d7a6b8605f809da34a
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-13 22:06:06 +0300

    Navigation Links and Side Drawer added to Main Nav

--- src/shared/components/Navigation/MainNavigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation/MainNavigation.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import MainHeader from '../MainHeader/MainHeader';
+import NavLinks from '../NavLinks/NavLinks';
+import SideDrawer from '../SideDrawer/SideDrawer';
 import './MainNavigation.css';
 
 const MainNavigation = (props) => {
   return (
-    <MainHeader>
-      <button className='main-navigation__menu-btn'>
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <h1 className='main-navigation__title'>
-        <Link to='/'>Your Places</Link>
-      </h1>
-      <nav>...</nav>
-    </MainHeader>
+    <Fragment>
+      <SideDrawer>
+        <nav className='main-navigation__drawer-nav'>
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className='main-navigation__menu-btn'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <h1 className='main-navigation__title'>
+          <Link to='/'>Your Places</Link>
+        </h1>
+        <nav className='main-navigation__header-nav'>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </Fragment>
   );
 };
 
